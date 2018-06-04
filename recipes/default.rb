5fd00ef66694942b4d7be7eb71d721c7ef0a6736
hieunba <hi3unb@gmail.com>
2018-06-05 04:35:32 +0700

    Update resource name

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-windows_package 'Acrobat Reader Installer' do
+windows_package 'Adobe Acrobat Reader DC' do
   source 'https://storage.googleapis.com/chef_files/Acrobat%20Reader.exe'
   checksum '0a24e18146acd79df7ba98a3ec6a3a95a5c156b182916381bccb2e0f1d54777a'
   installer_type :custom
