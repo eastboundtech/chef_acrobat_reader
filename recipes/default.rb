1fdef4833b1a6309f7d3fad67ad3f95ca65f605d
hieunba <hi3unb@gmail.com>
2018-06-05 04:23:18 +0700

    Add checksum

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,7 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 windows_package 'Acrobat Reader Installer' do
   source 'https://storage.googleapis.com/chef_files/Acrobat%20Reader.exe'
+  checksum '0a24e18146acd79df7ba98a3ec6a3a95a5c156b182916381bccb2e0f1d54777a'
   installer_type :custom
   options '/Q'
 end
