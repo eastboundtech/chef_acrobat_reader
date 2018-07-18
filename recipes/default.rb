c5646a9009a2c7f0b5341943018e735a8e7b1f45
Timothy Wright <timothy.g.wright@gmail.com>
2018-07-18 14:33:14 -0700

    Update default.rb
    changed to msi

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,9 +3,8 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-windows_package 'Adobe Acrobat Reader DC' do
-  source 'https://storage.googleapis.com/chef_files/Acrobat%20Reader.exe'
-  checksum '0a24e18146acd79df7ba98a3ec6a3a95a5c156b182916381bccb2e0f1d54777a'
-  installer_type :custom
-  options '/Q'
+windows_package 'Acrobat Reader' do
+  action :install
+  source 'https://storage.googleapis.com/chef_files/AcroRead.msi'
+  checksum 'AA7346A302394AC0F8E0063AC2B3EDDC661F48B543BB90F359DACCAFAD96701E'
 end
